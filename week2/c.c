f6d15edfc3b7b6c7668adb79ecba600217b55726
Yelshat Duskaliyev <ura2178@gmail.com>
2020-09-08 19:06:29 +0600

    fix week 2

--- week2/c.c
+++ week2/c.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
-int main() {
+int main(int argc, char* argv[]) {
+	if (argc < 2) {
+		printf("give the n as cli argument\n");
+		return 0;
+	}
     int n;
-    scanf("%d", &n);
+    sscanf(argv[1], "%d", &n);
+    // scanf("%d", &n);
     for (int i = n; i > 0; i--) {
         for (int j = i - 1; j > 0; j--) {
             printf(" ");
